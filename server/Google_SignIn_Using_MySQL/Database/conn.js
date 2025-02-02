510c4b51ebb1550a98b5dcaf0714913a38789a86
selvisentest2006 <selvisentest2006@gmail.com>
2025-02-02 17:36:24 +0530

    added port for sql

--- server/Google_SignIn_Using_MySQL/Database/conn.js
+++ server/Google_SignIn_Using_MySQL/Database/conn.js
@@ -4,7 +4,8 @@ const conn = mysql.createPool({
     host: process.env.host,
     user: process.env.user,
     password: process.env.password,
-    database: process.env.database
+    database: process.env.database,
+    port: 3306
 });
 
 conn.getConnection()
